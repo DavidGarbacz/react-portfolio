681ceef9fef3720951b8ba98f02db52ca7807acd
The Jailbreaker <th3j4ilbr34k3r@icloud.com>
2025-04-29 13:51:33 +0600

    chore: fix main.jsx imports, import CSS, add build & Netlify config

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,10 +1,11 @@
+// src/main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css'       // ← your Vite-built CSS
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
